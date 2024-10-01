2aa7fe276a78ada389a63dd6813a34e904bb3838
kelly <W0433053@nscc.ca>
2024-10-01 07:49:20 -0300

    Accordion implementation with some styles

--- src/components/pages/ProfilePage.jsx
+++ src/components/pages/ProfilePage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import { Disclosure, DisclosurePanel, DisclosureButton } from "@headlessui/react";
 import RecentReviewsContainer from "../containers/RecentReviewsContainer";
 import RecentOrdersContainer from "../containers/RecentOrdersContainer";
 import '../../styles/ProfilePage.css'
@@ -9,20 +10,43 @@ function ProfilePage({ user }) {
     return (
         <div className="profile-page">
             <h1>Welcome {user.email}</h1>
-            <ul>
+            <ul className="links">
                 <li><Link to="/review" className="cta">Leave a review!</Link></li>
                 <li><Link to="/order" className="cta">Make an order!</Link></li>
             </ul>
 
             <div className="profile-page-inner-wrapper">
                 <section>
-                    <p>Here are all of the orders you've made</p>
-                    <RecentOrdersContainer user={user}/>
+                    <Disclosure>
+                        {({ open }) => (
+                            <>
+                                <DisclosureButton style={{ borderRadius: "10px", fontFamily: "inherit", width: '100%', padding: '10px 15px', backgroundColor: '#f5f5f5', cursor: 'pointer', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                                    <h3>Here are all of the orders you've ever made</h3>
+                                    <span>{open ? '▲' : '▼'}</span>
+                                </DisclosureButton>
+                                <DisclosurePanel>
+                                    <RecentOrdersContainer user={user}/>
+                                </DisclosurePanel>
+                            </>
+                        )}
+                    </Disclosure>
                 </section>
 
                 <section>
-                    <p>Here are all of the reviews you've ever made</p>
-                    <RecentReviewsContainer user={user}/>
+
+                    <Disclosure>
+                        {({ open }) => (
+                            <>
+                                <DisclosureButton style={{ borderRadius: "10px", fontFamily: "inherit", width: '100%', padding: '10px 15px', backgroundColor: '#f5f5f5', cursor: 'pointer', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                                    <h3>Here are all of the reviews you've ever made</h3>
+                                    <span>{open ? '▲' : '▼'}</span>
+                                </DisclosureButton>
+                                <DisclosurePanel>
+                                    <RecentReviewsContainer user={user}/>
+                                </DisclosurePanel>
+                            </>
+                        )}
+                    </Disclosure>
                 </section>
             </div>
 
