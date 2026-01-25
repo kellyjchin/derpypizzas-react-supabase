f860417c2c830c66e48b5c12c3a2e9fa0d3e36c1
kelly <W0433053@nscc.ca>
2026-01-25 12:33:50 -0700

    REFACTOR: Moving Table of orders for a Profile from the ProfilePage.jsx into the RecentOrdersContainer.jsx and removing duplicate code

--- src/components/containers/RecentOrdersContainer.jsx
+++ src/components/containers/RecentOrdersContainer.jsx
@@ -1,8 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Order from "../Order";
-import { supabase } from "../../supabaseClient";
 import { fetchOrders } from "../../helpers";
-import { useLocation } from "react-router-dom";
 
 function RecentOrdersContainer({ user }) {
 
@@ -18,19 +15,43 @@ function RecentOrdersContainer({ user }) {
     }, []);
 
     return (
-        <div className="orders-container">
-            {orders ?
-                orders.map( (order, index) => (
-                    <Order
-                        orderData={order}
-                    /> 
-                ))
+        <section>
+            <h3>Your recent orders:</h3>
+            <table>
+                <thead>
+                    <th>Date</th>
+                    <th>For</th>
+                    <th>Delivery Address</th>
+                    <th>Size</th>
+                    <th>Toppings</th>
+                    <th>Qty.</th>
+                    <th>Total Price</th>
+                </thead>
+                <tbody>
+                    {orders ?
 
-                :
-                <p>Loading Orders...</p>
-            }
-            
-        </div>
+                        orders.map( (order, index) => {
+                            const dateCreated = new Date(order.created_at);
+                            const {name, delivery_address, pizza_size, toppings, quantity, total_price} = order;
+                            return(
+                                <tr>
+                                    <td>{dateCreated.toLocaleDateString()}</td>
+                                    <td>{name}</td>
+                                    <td>{delivery_address}</td>
+                                    <td>{pizza_size}</td>
+                                    <td>{toppings ? toppings.join(', ') : "no toppings"}</td>
+                                    <td>{quantity}</td>
+                                    <td>{total_price.toFixed(2)}</td>
+                                </tr>
+                            )
+                        })
+
+                        :
+                        <tr><td>Loading Orders...</td></tr>
+                    }
+                </tbody>
+            </table>
+        </section>
     );
 }
 
--- src/components/pages/ProfilePage.jsx
+++ src/components/pages/ProfilePage.jsx
@@ -1,23 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { Link } from 'react-router-dom';
-import { Disclosure, DisclosurePanel, DisclosureButton } from "@headlessui/react";
-import RecentReviewsContainer from "../containers/RecentReviewsContainer";
+// import RecentReviewsContainer from "../containers/RecentReviewsContainer";
 import RecentOrdersContainer from "../containers/RecentOrdersContainer";
 import '../../styles/ProfilePage.css'
-import { fetchOrders, fetchRewardBalance } from "../../helpers";
+import { fetchRewardBalance } from "../../helpers";
 
 function ProfilePage({ user }) {
-    const [orders, setOrders] = useState([]);
-    useEffect( () => {
-        async function getOrders() {
-            let ordersData = await fetchOrders(user.email);
-            if(ordersData) {
-                setOrders(ordersData);
-            }
-        }
-        getOrders();
-    }, []);
-
     const [rewardBalance, setRewardBalance] = useState(0);
     useEffect( () => {
         const getRewardBalance = async () => {
@@ -39,46 +27,8 @@ function ProfilePage({ user }) {
             </ul>
 
             <div className="profile-page-inner-wrapper">
-                <section>
-                    <h3>Your recent orders:</h3>
-                    <table>
-                        <thead>
-                            <th>Date</th>
-                            <th>For</th>
-                            <th>Delivery Address</th>
-                            <th>Size</th>
-                            <th>Toppings</th>
-                            <th>Qty.</th>
-                            <th>Total Price</th>
-                        </thead>
-                        <tbody>
-                            {orders ?
-
-                                orders.map( (order, index) => {
-                                    const dateCreated = new Date(order.created_at);
-                                    
-                                    return(
-                                        <tr>
-                                            <td>{dateCreated.toLocaleDateString()}</td>
-                                            <td>{order.name}</td>
-                                            <td>{order.delivery_address}</td>
-                                            <td>{order.pizza_size}</td>
-                                            <td>{order.toppings ? order.toppings.join(', ') : "no toppings"}</td>
-                                            <td>{order.quantity}</td>
-                                            <td>{order.total_price.toFixed(2)}</td>
-                                        </tr>
-                                    )
-                                })
-
-                                :
-                                <tr><td>Loading Orders...</td></tr>
-                            }
-                        </tbody>
-                    </table>
-                </section>
+                <RecentOrdersContainer user={user}/>
             </div>
-
-
         </div>
 
     );
