800220f1deb15ae5da1b2221462090713d2876e7
kelly <W0433053@nscc.ca>
2026-02-04 14:24:25 -0700

    Adding loading skeleton functionality for Recent Reviews on homepage

---
+++ src/components/ReviewSkeleton.jsx
@@ -0,0 +1,11 @@
+function ReviewSkeleton() {
+
+    return (
+        <div className="review-skeleton">
+
+        </div>
+    );
+
+}
+
+export default ReviewSkeleton;
--- src/components/containers/RecentReviewsContainer.jsx
+++ src/components/containers/RecentReviewsContainer.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Review from "../Review";
 // import { supabase } from "../../supabaseClient";
 import { fetchReviews } from "../../helpers";
 import { useLocation } from "react-router-dom";
+import ReviewSkeleton from "../ReviewSkeleton";
 
 function RecentReviewsContainer({ user }) {
     // TODO: Trim Body Review if it gets really long. Let's say 100 characters maybe?
@@ -10,46 +11,60 @@ function RecentReviewsContainer({ user }) {
     const currentUrl = locationObj.pathname;
 
     const [reviews, setReviews] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
     useEffect( () => {
+        let isMounted = true;
+
         async function getReviews() {
+            setIsLoading(true);
             let reviewData;
             if (currentUrl === "/") {
                 reviewData = await fetchReviews();
             }
 
-            if (currentUrl === "/profile") {
+            if (currentUrl === "/profile" && user?.email) {
                 reviewData = await fetchReviews(user.email);
             }
 
-            if(reviewData) {
-                setReviews(reviewData);
+            if (isMounted) {
+                setReviews(reviewData || []);
+                setIsLoading(false);
             }
         }
         getReviews();
+        return () => {
+            isMounted = false;
+        };
     }, [currentUrl, user]);
 
     return (
         <div className="reviews-container">
             { currentUrl === "/" && <h2>Recent Reviews</h2>}
-            {reviews ?
-                reviews.map( (review, index) => (
-                    <Review
-                        key={index}
-                        username={review.reviewer}
-                        body={review.review_body}
-                        rating={review.rating}
-                        date={'derp'}
-                        currentUrl={currentUrl}
-                        reviewId={review.id}
-                        user={user}
-                        inLikeCount={review.likeCount}
-                        inDislikeCount={review.dislikeCount}
-                    /> 
-                ))
 
-                :
-                <p>Loading Reviews...</p>
+            { isLoading ? (
+                <>
+                    <ReviewSkeleton/>
+                    <ReviewSkeleton/>
+                    <ReviewSkeleton/>
+                </>
+                ) : (
+                    reviews.map( (review, index) => (
+                        <Review
+                            key={index}
+                            username={review.reviewer}
+                            body={review.review_body}
+                            rating={review.rating}
+                            date={'derp'}
+                            currentUrl={currentUrl}
+                            reviewId={review.id}
+                            user={user}
+                            inLikeCount={review.likeCount}
+                            inDislikeCount={review.dislikeCount}
+                        /> 
+                    ))
+                )
             }
+
             
         </div>
     );
